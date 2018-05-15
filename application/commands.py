68db5b7bfde561f30236a1b6c72768e94dcb0ed6
Adam Shimali <adam.shimali@gmail.com>
2018-05-15 17:36:28 +0100

    Yeah don't comment out the useful stuff

--- application/commands.py
+++ application/commands.py
@@ -45,77 +45,70 @@ def load_everything():
                 print(e)
                 db.session.rollback()
 
-    # areas = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/area/index.tsv'
-    # with closing(requests.get(areas, stream=True)) as r:
-    #     reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
-    #     for row in reader:
-    #         area_url = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/area/%s' % row['path']
-    #         area_data = requests.get(area_url).json()
-    #         for feature in area_data['features']:
-    #             if feature.get('type') is not None \
-    #                     and feature.get('type') == 'Feature'\
-    #                     and feature.get('properties').get('area') is not None:
-    #                 area_id = feature['properties']['area']
-    #                 area = Area(area=area_id, data=feature)
-    #                 try:
-    #                     db.session.add(area)
-    #                     db.session.commit()
-    #                 except Exception as e:
-    #                     print(e)
-    #                     db.session.rollback()
-    #
-    # organisations = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/organisation.tsv'
-    # with closing(requests.get(organisations, stream=True)) as r:
-    #     reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
-    #     for row in reader:
-    #         org = Organisation(organisation=row['organisation'],
-    #                            name=row['name'],
-    #                            website=row['website'])
-    #         if row.get('area'):
-    #             area = db.session.query(Area).get(row.get('area'))
-    #             org.area = area
-    #         try:
-    #             db.session.add(org)
-    #             db.session.commit()
-    #         except Exception as e:
-    #             print(e)
-    #             db.session.rollback()
-    #
-    # publications = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/publication/index.tsv'
-    # with closing(requests.get(publications, stream=True)) as r:
-    #     reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
-    #     for row in reader:
-    #         publication_url = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/publication/%s' % row['path']
-    #         publication_data = requests.get(publication_url).content.decode('utf-8')
-    #         md = markdown.Markdown(extensions=['markdown.extensions.meta'])
-    #         md.convert(publication_data)
-    #         publication = Publication(publication=md.Meta['publication'][0],
-    #                                   name=md.Meta['name'][0],
-    #                                   url=md.Meta['documentation-url'][0],
-    #                                   data_url=md.Meta['data-url'][0])
-    #
-    #         organisation = db.session.query(Organisation).get(md.Meta['organisation'][0])
-    #         if organisation is not None:
-    #             publication.organisation = organisation
-    #
-    #         lic = db.session.query(Licence).get(md.Meta['licence'][0])
-    #         if lic is not None:
-    #             publication.licence = lic
-    #
-    #         try:
-    #             db.session.add(publication)
-    #             db.session.commit()
-    #
-    #         except Exception as e:
-    #             print(e)
-    #             db.session.rollback()
+    areas = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/area/index.tsv'
+    with closing(requests.get(areas, stream=True)) as r:
+        reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
+        for row in reader:
+            area_url = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/area/%s' % row['path']
+            area_data = requests.get(area_url).json()
+            for feature in area_data['features']:
+                if feature.get('type') is not None \
+                        and feature.get('type') == 'Feature'\
+                        and feature.get('properties').get('area') is not None:
+                    area_id = feature['properties']['area']
+                    area = Area(area=area_id, data=feature)
+                    try:
+                        db.session.add(area)
+                        db.session.commit()
+                    except Exception as e:
+                        print(e)
+                        db.session.rollback()
 
+    organisations = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/organisation.tsv'
+    with closing(requests.get(organisations, stream=True)) as r:
+        reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
+        for row in reader:
+            org = Organisation(organisation=row['organisation'],
+                               name=row['name'],
+                               website=row['website'])
+            if row.get('area'):
+                area = db.session.query(Area).get(row.get('area'))
+                org.area = area
+            try:
+                db.session.add(org)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                db.session.rollback()
 
-    # print('Loaded...')
-    # print(db.session.query(func.count(Area.area)).one(), 'areas')
-    # print(db.session.query(func.count(Organisation.organisation)).one(), 'organisations')
-    # print(db.session.query(func.count(Publication.publication)).one(), 'publications')
+    publications = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/publication/index.tsv'
+    with closing(requests.get(publications, stream=True)) as r:
+        reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
+        for row in reader:
+            publication_url = 'https://raw.githubusercontent.com/communitiesuk/digital-land-data/master/data/publication/%s' % row['path']
+            publication_data = requests.get(publication_url).content.decode('utf-8')
+            md = markdown.Markdown(extensions=['markdown.extensions.meta'])
+            md.convert(publication_data)
+            publication = Publication(publication=md.Meta['publication'][0],
+                                      name=md.Meta['name'][0],
+                                      url=md.Meta['documentation-url'][0],
+                                      data_url=md.Meta['data-url'][0])
 
+            organisation = db.session.query(Organisation).get(md.Meta['organisation'][0])
+            if organisation is not None:
+                publication.organisation = organisation
+
+            lic = db.session.query(Licence).get(md.Meta['licence'][0])
+            if lic is not None:
+                publication.licence = lic
+
+            try:
+                db.session.add(publication)
+                db.session.commit()
+
+            except Exception as e:
+                print(e)
+                db.session.rollback()
 
     # TODO copyright, licence, geography. the body text as well?
 
