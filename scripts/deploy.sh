287822b5e0a6f8491bced3febbeb658154951c83
Adam Shimali <adam.shimali@gmail.com>
2018-05-23 10:49:53 +0100

    install cloudfoundry cli

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+# nicked from here https://github.com/18F/cf-cd-example/blob/master/deploy.sh
+
 set -e
+
+unamestr=`uname`
+
+if ! hash cf 2>/dev/null; then
+  echo "Installing Cloud Foundry CLI..."
+  if [[ "$unamestr" == 'Darwin' ]]; then
+    brew tap cloudfoundry/tap
+    brew install cf-cli
+  else
+    mkdir -p tmp
+    PATH=$PWD/tmp:$PATH
+    curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx -C tmp
+  fi
+fi
+
 
 if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
   cf login -a "$CF_API" -u "$CF_USERNAME" -p "$CF_PASSWORD"
