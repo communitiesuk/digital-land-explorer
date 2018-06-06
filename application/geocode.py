056f46cebf52aff67ad456ff0b39ea9c13f21230
Colm Britton <colmjude@gmail.com>
2018-06-06 12:37:41 +0800

    add reverse geocode func, lat/lng to addr

--- application/geocode.py
+++ application/geocode.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 def nomgeocode(query):
   # send the query to the nominatim geocoder and parse the json response
@@ -20,5 +21,41 @@ def nomgeocode(query):
   else:
     geo_result['success'] = False
     geo_result['msg'] = "Failed to return lat/lng for query:{}".format(query)
+
+  return geo_result
+
+def _to_readable_addr(addr_obj):
+  addr_str = ""
+  if 'road' in addr_obj.keys():
+    addr_str += addr_obj['road']
+
+  if 'suburb' in addr_obj.keys():
+    addr_str += ', ' + addr_obj['suburb']
+  elif 'town' in addr_obj.keys():
+    addr_str += ', ' + addr_obj['town']
+
+  return addr_str
+
+def nom_reverse_geocode(lat, lng):
+  url_template = ' https://nominatim.openstreetmap.org/reverse?format=json&lat={0}&lon={1}&zoom=18&addressdetails=1'
+  url = url_template.format(lat, lng)
+  response = requests.get(url, timeout=60)
+
+  geo_result = {
+    "success": True,
+    "lat": lat,
+    "lng": lng
+  }
+
+  if response.status_code is not 200:
+    geo_result['success'] = False
+    geo_result['msg'] = "Failed to return a results for lat:{} lng:{}".format(lat, lng)
+    return geo_result
+  
+  result = response.json()
+
+  if result and 'lat' in result:
+    geo_result['display_name'] = result['display_name']
+    geo_result['address'] = _to_readable_addr(result['address'])
 
   return geo_result
