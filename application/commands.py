f7f0fc4e528ec06d71ad28e1bc863ced68bd2aba
Adam Shimali <adam.shimali@gmail.com>
2018-07-09 16:04:32 +0100

    This includes:
    1) attempts to speed up loading
    2) zero downtime deploys

--- application/commands.py
+++ application/commands.py
@@ -6,10 +6,14 @@ import requests
 import csv
 import platform
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from urllib.request import urlopen
 from contextlib import closing
 from flask.cli import with_appcontext
 from ijson import common
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from application.models import Organisation, Feature, Publication, Licence, Copyright, organisation_feature
 from application.extensions import db
 
@@ -21,11 +25,6 @@ else:
 
 
 json_to_geo_query = "SELECT ST_SetSRID(ST_GeomFromGeoJSON('%s'), 4326);"
-repo = os.getenv('DATA_REPO', 'https://raw.githubusercontent.com/communitiesuk/digital-land-collector')
-branch = os.getenv('BRANCH', 'master')
-s3_region = os.getenv('S3_REGION')
-s3_bucket = os.getenv('S3_BUCKET')
-s3_bucket_url = 'http://%s.s3.amazonaws.com' % s3_bucket
 
 
 def floaten(event):
@@ -33,33 +32,6 @@ def floaten(event):
         return (event[0], event[1], float(event[2]))
     else:
         return event
-
-
-def process_file(file_url, org_feature_mappings):
-    print('Processing', file_url)
-    try:
-        f = urlopen(file_url)
-        events = map(floaten, ijson.parse(f))
-        data = common.items(events, 'features.item')
-        for feature in data:
-            id = feature['properties'].get('feature')
-            item = 'item:%s' % feature['properties'].get('item')
-            publication = feature['properties'].get('publication')
-            feature_id = id if id is not None else item
-
-            if db.session.query(Feature).get(feature_id) is None:
-                geo = json.dumps(feature['geometry'])
-                geometry = db.session.execute(json_to_geo_query % geo).fetchone()[0]
-                f = Feature(feature=feature_id, data=feature, geometry=geometry, item=item, publication=publication)
-                if feature_id in org_feature_mappings:
-                    org = Organisation.query.get(org_feature_mappings[feature_id])
-                    org.feature = f
-                db.session.add(f)
-                db.session.commit()
-        print('Done', file_url)
-    except Exception as e:
-        print(e)
-        print('Error loading', file_url)
 
 
 def _handle_markdown(item, contents):
@@ -118,17 +90,15 @@ def _handle_organisation(contents, org_feature_mappings):
         return 0
 
 
-def _load_features(org_feature_mappings):
-    s3 = boto3.resource('s3', region_name=s3_region)
-    bucket = s3.Bucket(s3_bucket)
-    for file in bucket.objects.all():
-        file_url = '%s/%s' % (s3_bucket_url, file.key)
-        process_file(file_url, org_feature_mappings)
-
-
 @click.command()
 @with_appcontext
 def load_everything():
+
+    from flask import current_app
+
+    s3_region = current_app.config.S3_REGION
+    s3_bucket = current_app.config.S3_BUCKET
+    s3_bucket_url = 'http://%s.s3.amazonaws.com' % s3_bucket
 
     print('Loading the entire universe')
 
@@ -139,18 +109,18 @@ def load_everything():
 
     for item in items:
         if item in ['licence', 'copyright', 'publication']:
-            item_url = '%s/%s/data/%s/index.tsv' % (repo, branch, item)
+            item_url = '%s/%s/index.tsv' % (s3_bucket_url, item)
             print('Loading', item_url)
             with closing(requests.get(item_url, stream=True)) as r:
                 reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
                 for row in reader:
-                    file_url = '%s/%s/data/%s/%s' % (repo, branch, item, row['path'])
+                    file_url = '%s/%s/%s' % (s3_bucket_url, item, row['path'])
                     print('Processing', file_url)
                     contents = requests.get(file_url).content.decode('utf-8')
                     count += _handle_markdown(item, contents)
 
         elif item == 'organisation':
-            item_url = '%s/%s/data/organisation.tsv' % (repo, branch)
+            item_url = '%s/organisation.tsv' % s3_bucket_url
             print('Loading', item_url)
             with closing(requests.get(item_url, stream=True)) as r:
                 reader = csv.DictReader(r.iter_lines(decode_unicode=True), delimiter='\t')
@@ -161,12 +131,101 @@ def load_everything():
         count = 0
 
     try:
-        _load_features(org_feature_mappings)
+        _load_features(org_feature_mappings, s3_bucket, s3_region)
     finally:
         db.session.execute('CLUSTER feature USING idx_feature_geometry')
         # db.session.execute('VACUUM ANALYZE feature;')
 
     print('Done')
+
+
+def _load_features(org_feature_mappings, s3_bucket, s3_region):
+
+    s3_bucket_url = 'http://%s.s3.amazonaws.com' % s3_bucket
+    s3 = boto3.resource('s3', region_name=s3_region)
+    bucket = s3.Bucket(s3_bucket)
+    urls = ['%s/%s' % (s3_bucket_url, file.key) for file in bucket.objects.filter(Prefix='feature')]
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(process_file, url, org_feature_mappings) for url in urls]
+        for future in as_completed(futures):
+            print(future.result())
+
+
+@click.command()
+@click.option('--file')
+@with_appcontext
+def load_local_file(file):
+    process_file(file, {})
+
+
+def process_file(file_url, org_feature_mappings):
+    print('Loading', file_url)
+    some_engine = create_engine(os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost/digital_land'))
+    Session = sessionmaker(bind=some_engine)
+    session = Session()
+    total = 0
+    try:
+        if file_url.startswith('http'):
+            f = urlopen(file_url)
+        else:
+            f = open(file_url, 'rb')
+        events = map(floaten, ijson.parse(f))
+        data = common.items(events, 'features.item')
+        records = []
+        orgs_to_save = []
+        processed = set([])
+
+        for feature in data:
+            id = feature['properties'].get('feature')
+            item = 'item:%s' % feature['properties'].get('item')
+            publication = feature['properties'].get('publication')
+            feature_id = id if id is not None else item
+
+            if session.query(Feature).get(feature_id) is None and feature_id not in processed:
+                geo = json.dumps(feature['geometry'])
+                geometry = session.execute(json_to_geo_query % geo).fetchone()[0]
+
+                if feature_id in org_feature_mappings:
+                    org = session.query(Organisation).get(org_feature_mappings[feature_id])
+                    org.feature_id = feature_id
+                    orgs_to_save.append(org)
+
+                records.append(dict(feature=feature_id,
+                                    data=feature,
+                                    geometry=geometry,
+                                    item=item,
+                                    publication=publication))
+
+                processed.add(feature_id)
+
+                if len(records) % 10000 == 0:
+                    session.bulk_insert_mappings(Feature, records)
+                    session.bulk_save_objects(orgs_to_save)
+                    session.commit()
+                    print('Saved', len(records), 'features from', file_url)
+                    total += len(records)
+                    records = []
+                    orgs_to_save = []
+
+        session.bulk_insert_mappings(Feature, records)
+        session.bulk_save_objects(orgs_to_save)
+        session.commit()
+
+        print('Saved', len(records), 'features from', file_url)
+        total += len(records)
+        print('Finished loading', file_url)
+
+    except Exception as e:
+        print(e)
+        print('Error loading', file_url)
+    finally:
+        try:
+            f.close()
+        except:
+            pass
+
+    return 'Loaded total of %d features from %s' % (total, file_url)
 
 
 @click.command()
--- config/config.py
+++ config/config.py
@@ -6,6 +6,10 @@ import os
 if os.environ.get('VCAP_SERVICES') is not None:
     vcap_services = json.loads(os.environ.get('VCAP_SERVICES'))
     os.environ['SQLALCHEMY_DATABASE_URI'] = vcap_services['postgres'][0]['credentials']['uri']
+    os.environ['SECRET_KEY'] = vcap_services['user-provided'][0]['credentials']['SECRET_KEY']
+    os.environ['MAPBOX_TOKEN'] = vcap_services['user-provided'][0]['credentials']['MAPBOX_TOKEN']
+    os.environ['S3_REGION'] = vcap_services['user-provided'][0]['credentials']['S3_REGION']
+    os.environ['S3_BUCKET'] = vcap_services['user-provided'][0]['credentials']['S3_BUCKET']
 
 class Config(object):
     APP_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -14,6 +18,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     MAPBOX_TOKEN=os.getenv('MAPBOX_TOKEN')
+    S3_REGION=os.getenv('S3_REGION')
+    S3_BUCKET=os.getenv('S3_BUCKET')
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
